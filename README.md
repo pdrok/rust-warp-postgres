6638db99428a9017c65a130bd9ffada9950a8d7b
Pedro Lopez <plopez@poweranalytics.com>
2022-09-27 15:50:04 -0400

    update cargo package and fixed typo on db.sql

---
+++ README.md
@@ -0,0 +1,15 @@
+## Rust-warp-postgres
+
+### An async CRUD web service in Rust with warp
+
+run code:
+
+```sh
+cargo run
+```
+
+run the database:
+
+```sh
+docker run -p 7878:5432 -d postgres:9.6.12
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-use warp::{http::StatusCode, Filter};
+use mobc::{Connection, Pool};
+use mobc_postgres::{tokio_postgres, PgConnectionManager};
+use std::convert::Infallible;
+use tokio_postgres::NoTls;
+use warp::Filter;
+
+type DBCon = Connection<PgConnectionManager<NoTls>>;
+type DBPool = Pool<PgConnectionManager<NoTls>>;
+type Result<T> = std::result::Result<T, warp::Rejection>;
 
 mod data;
 mod db;
@@ -7,9 +15,47 @@ mod handler;
 
 #[tokio::main]
 async fn main() {
-    let health_route = warp::path!("health").map(|| StatusCode::OK);
+    let db_pool = db::create_pool().expect("database pool can be created");
 
-    let routes = health_route.with(warp::cors().allow_any_origin());
+    db::init_db(&db_pool)
+        .await
+        .expect("database can be initialized");
+
+    let health_route = warp::path!("health")
+        .and(with_db(db_pool.clone()))
+        .and_then(handler::health_handler);
+
+    let todo = warp::path("todo");
+    let todo_routes = todo
+        .and(warp::get())
+        .and(warp::body::json())
+        .and(with_db(db_pool.clone()))
+        .and_then(handler::list_todos_handler)
+        .or(todo
+            .and(warp::post())
+            .and(warp::body::json())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::create_todo_handler))
+        .or(todo
+            .and(warp::put())
+            .and(warp::path::param())
+            .and(warp::body::json())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::update_todo_handler))
+        .or(todo
+            .and(warp::delete())
+            .and(warp::path::param())
+            .and(with_db(db_pool.clone()))
+            .and_then(handler::delete_todo_handler));
+
+    let routes = health_route
+        .or(todo_routes)
+        .with(warp::cors().allow_any_origin())
+        .recover(error::handle_rejection);
 
     warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+}
+
+fn with_db(db_pool: DBPool) -> impl Filter<Extract = (DBPool,), Error = Infallible> + Clone {
+    warp::any().map(move || db_pool.clone())
 }
